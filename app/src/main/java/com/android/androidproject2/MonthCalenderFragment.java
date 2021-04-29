42d74349bdab0f21710d9ed7b3d198f7fbac6484
kyj0619 <kyj97yj@gmail.com>
2021-04-29 16:37:54 +0900

    Initial commit

---
+++ app/src/main/java/com/android/androidproject2/MonthCalenderFragment.java
@@ -0,0 +1,62 @@
+package com.android.androidproject2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MonthCalenderFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MonthCalenderFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private int mParam1;
+    private int mParam2;
+
+    public MonthCalenderFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MonthCalenderFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MonthCalenderFragment newInstance(int year, int month) {
+        MonthCalenderFragment fragment = new MonthCalenderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, year);
+        args.putInt(ARG_PARAM2, month);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+            mParam2 = getArguments().getInt(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_month_calender, container, false);
+    }
+}
